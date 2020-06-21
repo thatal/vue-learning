494445a7e71d106fbdfcde1a37b680046b79332f
Sunil Thatal <sunilthatal007@gmail.com>
2020-06-21 17:59:15 +0530

    vue learnig component with npm and laravel mix

---
+++ src/components/Accordion.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{item.title}}</p>
+        <p @click="toggle = !toggle">Details</p>
+        <p v-if="toggle">{{item.description}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Accordion',
+    props: ['item'],
+    data: function() {
+        return {
+            toggle: false,
+        }
+    },
+}
+</script>
